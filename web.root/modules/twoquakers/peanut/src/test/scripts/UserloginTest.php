7e570a3963d3831c6d8f95637e14d06c80fd8f4c
Terry SoRelle <tls@2quakers.net>
2018-01-15 11:46:54 -0600

    implemented user->signIn

--- web.root/modules/twoquakers/peanut/src/test/scripts/UserloginTest.php
+++ web.root/modules/twoquakers/peanut/src/test/scripts/UserloginTest.php
@@ -28,15 +28,18 @@ class UserloginTest extends TestScript
         $this->assert($actual === false,'Sign in result should have been false');
 */
 
-        $actual = TUser::SignIn('unknownuser','bad password');
-        $this->assert($actual === false,'Sign in result should have been false');
-
-        $expected = !TUser::getCurrent()->isAuthenticated();
-        $this->assert($expected,'Expected anonymous');
+//        $actual = TUser::SignIn('unknownuser','bad password');
+//        $this->assert($actual === false,'Sign in result should have been false');
+//
+//        $expected = !TUser::getCurrent()->isAuthenticated();
+//        $this->assert($expected,'Expected anonymous');
 
         $actual = TUser::SignIn($username,'M6tJb@1*yoIf97cFCQlDFmwJ');
         $this->assert($actual,'Not logged in');
+        $current = TUser::getCurrent();
         $actual = TUser::getCurrent()->getUserName();
         $this->assertEquals($username,$actual,'Wrong user is current');
+        $actual = $current->isAdmin();
+        $this->assert($actual,'Not admin');
     }
 }
